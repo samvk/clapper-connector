330f8c6b76aa2970fe9026154160f70145281b34
Sam Kauffman <samvnkauffman@gmail.com>
2019-01-19 17:30:44 -0500

    WIP::add support for clapping

--- functions/util.js
+++ functions/util.js
@@ -1,5 +1,9 @@
 const randomPop = (arr) => arr[Math.floor(Math.random() * arr.length)];
 module.exports.randomPop = randomPop;
 
+module.exports.sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+module.exports.hostingPath = (path) => `https://the-clapper-cd63b.firebaseapp.com/${path}`;
+
 // common phrases
 module.exports.sayOkay = () => randomPop(['Ok', 'Sure', 'Alright']);

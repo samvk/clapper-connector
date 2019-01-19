f1149639e5804beeb97d77a4ca9454c3ad4cdde0
Sam Kauffman <samvnkauffman@gmail.com>
2019-01-19 17:45:23 -0500

    simply sleep method also some small timing and quote changes

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,14 @@
 const { dialogflow, SimpleResponse, BasicCard, Image, Suggestions } = require('actions-on-google');
 const functions = require('firebase-functions');
-const { randomPop, sayOkay, sleep, hostingPath } = require('./util');
+const { randomPop, sayOkay, hostingPath } = require('./util');
 
 const app = dialogflow({ debug: true });
 
 const clapperSuggestions = new Suggestions(['Turn on the lights', 'Power off my TV', '👏']);
 
 app.intent('Default Welcome Intent', async (conv) => {
-    conv.ask(`<speak><voice gender="male"><prosody rate="90%" pitch="-2st">Pairing...</prosody></voice></speak>`);
-
-    // CHANGEME this is making it wait at the start, not the middle
-    await sleep(3000);
-
-    conv.ask(`<speak><voice gender="female">Connected!  \n${randomPop(['What would you like me to do?', 'What can I do for you?'])}</voice></speak>`);
+    conv.ask(`<speak><voice gender='male'><prosody rate='90%' pitch='-2st'>Pairing...</prosody></voice><break time='300ms' /></speak>`);
+    conv.ask(`<speak><voice gender='female'>Connected!  \n${randomPop(['What would you like me to do?', 'What can I do for you?'])}</voice></speak>`);
     conv.ask(clapperSuggestions);
 });
 
@@ -29,7 +25,7 @@ app.intent(['Default Fallback Intent', 'clapper_connector'], (conv) => {
         new SimpleResponse({
             text: okayPhrase,
             // CHANGEME needs to be a soundbite
-            speech: `<speak>${okayPhrase}!<break time='500ms' /><audio src='${hostingPath('audio/clap-1.mp3')}' /></speak>`,
+            speech: `<speak>${okayPhrase}!<break time='450ms' /><audio src='${hostingPath('audio/clap-1.mp3')}' /></speak>`,
         }),
         new BasicCard({
             // CHANGEME need to fix sizing of this image
--- functions/util.js
+++ functions/util.js
@@ -1,8 +1,6 @@
 const randomPop = (arr) => arr[Math.floor(Math.random() * arr.length)];
 module.exports.randomPop = randomPop;
 
-module.exports.sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
 module.exports.hostingPath = (path) => `https://the-clapper-cd63b.firebaseapp.com/${path}`;
 
 // common phrases
